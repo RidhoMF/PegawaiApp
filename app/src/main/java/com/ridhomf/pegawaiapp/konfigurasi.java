edad4c6c28ea78c733e95d848cacd4910ac5a964
RidhoMF <ridhofajri80@gmail.com>
2020-10-14 20:06:53 +0700

    Initial commit

---
+++ app/src/main/java/com/ridhomf/pegawaiapp/konfigurasi.java
@@ -0,0 +1,12 @@
+package com.ridhomf.pegawaiapp;
+
+public class konfigurasi {
+
+    public static String BASE_URL="http://192.168.1.2/pegawai/";
+    public static final String URL_ADD=BASE_URL+"/tambahpgw.php";
+    public static final String URL_GET_ALL=BASE_URL+"/tampilsemuapgw.php";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID="id";
+    public static final String TAG_NAME="name";
+}
